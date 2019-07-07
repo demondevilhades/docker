f4e3ae9cd803cd6c67b310db25a8e911b2742bbe
demondevilhades <demonhades@foxmail.com>
2019-07-08 03:43:29 +0800

    Update ubuntu16.sh

--- install/ubuntu16.sh
+++ install/ubuntu16.sh
@@ -4,7 +4,9 @@
 apt-get update 
 apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - 
+# curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add - 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
+# add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable" 
 apt-get update 
 
 apt-get install -y --no-install-recommends docker-ce 
