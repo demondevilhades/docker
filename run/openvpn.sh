b8014112b3b54ea7ef2f31ba69ea362e51e5b45b
demondevilhades <demonhades@foxmail.com>
2019-07-08 11:14:08 +0800

    Update openvpn.sh

--- run/openvpn.sh
+++ run/openvpn.sh
@@ -7,26 +7,25 @@ IP=***
 
 mkdir $OPENVPN_DATA
 
-# 1. gen config (-u udp://8.8.8.8)
-docker run --name openvpn-test --rm -it -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn:2.4 opvn_genconfig -u tcp://$IP 
+# 1. gen config 
+docker run --name openvpn-test --rm -it --log-driver=none -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn /usr/local/bin/ovpn_genconfig -u tcp://$IP 
 
-# 2. gen private key
-docker run --name openvpn-test --rm -it -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn:2.4 opvn_initpki 
+# 2. gen private key 
+docker run --name openvpn-test --rm -it --log-driver=none -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn /usr/local/bin/ovpn_initpki 
 #PEM password
 #PEM password
 #ignore
 #PEM password
 
-# 3. gen client key
-docker run --name openvpn-test --rm -it -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn:2.4 easyrsa build-client-full $OPENVPN_NAME nopass 
+# 3. gen client key 
+docker run --name openvpn-test --rm -it -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn /usr/local/bin/easyrsa build-client-full $OPENVPN_NAME nopass 
 #PEM password
 
-# 4. export client config
-docker run --name openvpn-test --rm -it -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn:2.4 opvn_getclient $OPENVPN_NAME > $OPENVPN_DATA/$OPENVPN_NAME.ovpn 
+# 4. export client config 
+docker run --name openvpn-test --rm -it -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn /usr/local/bin/ovpn_getclient $OPENVPN_NAME > $OPENVPN_DATA/$OPENVPN_NAME.ovpn 
 
-# 5. run openvpn docker container
-docker run -itd --name openvpn-test --privileged -v $OPENVPN_DATA:/etc/openvpn -p 1194:1194 kylemanna/openvpn:2.4 
+# 5. run openvpn docker container 
+docker run -itd --name openvpn-test --privileged -v $OPENVPN_DATA:/etc/openvpn -p 1194:1194 kylemanna/openvpn 
 
 # 
-
 
