bc3e007969f743214edccb1236def5b236409023
demondevilhades <demonhades@foxmail.com>
2019-07-08 16:28:30 +0800

    Update openvpn.sh

--- run/openvpn.sh
+++ run/openvpn.sh
@@ -1,14 +1,14 @@
 
 # to check 
 
-OPENVPN_DATA=***\openvpndata 
+OPENVPN_DATA=***/openvpndata 
 OPENVPN_NAME=*** 
 IP=*** 
 
 mkdir $OPENVPN_DATA
 
 # 1. gen config 
-docker run --name openvpn-test --rm -it --log-driver=none -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn /usr/local/bin/ovpn_genconfig -u tcp://$IP 
+docker run --name openvpn-test --rm -it --log-driver=none -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn /usr/local/bin/ovpn_genconfig -u udp://$IP 
 
 # 2. gen private key 
 docker run --name openvpn-test --rm -it --log-driver=none -v $OPENVPN_DATA:/etc/openvpn kylemanna/openvpn /usr/local/bin/ovpn_initpki 
@@ -26,6 +26,3 @@ docker run --name openvpn-test --rm -it -v $OPENVPN_DATA:/etc/openvpn kylemanna/
 
 # 5. run openvpn docker container 
 docker run -itd --name openvpn-test --privileged -v $OPENVPN_DATA:/etc/openvpn -p 1194:1194 kylemanna/openvpn 
-
-# 
-
