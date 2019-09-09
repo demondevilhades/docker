1e9c1c51bf90b94774857301bead57680691f14c
demondevilhades <demondevilhades@foxmail.com>
2019-09-09 09:21:13 +0800

    add bat and sh

--- run/mongo/mongo.bat
+++ run/mongo/mongo.bat
@@ -1,6 +1,4 @@
 
-rem windows
-
 docker volume create mongo_data
 
 docker run -itd --name mongo-dev -v mongo_data:/data/db -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=root mongo:4.2.0
@@ -8,3 +6,8 @@ docker run -itd --name mongo-dev -v mongo_data:/data/db -p 27017:27017 -e MONGO_
 docker run -itd --name mongo-express-dev -p 8081:8081 --link mongo-dev:mongo -e ME_CONFIG_MONGODB_SERVER=some-mongo -e ME_CONFIG_BASICAUTH_USERNAME=admin -e ME_CONFIG_BASICAUTH_PASSWORD=admin -e ME_CONFIG_MONGODB_ADMINUSERNAME=root -e ME_CONFIG_MONGODB_ADMINPASSWORD=root -e ME_CONFIG_MONGODB_SERVER=mongo -e ME_CONFIG_MONGODB_PORT=27017 mongo-express
 
 rem http://localhost:8081/
+docker exec -it mongo-dev bash
+mongo localhost:27017/admin -u root -p root
+
+use app_db
+db.createUser( {user: "app", pwd: "app", roles: [ { role: "readWrite", db: "app_db" } ]  } )
--- run/mysql.sh
+++ run/mysql.sh
@@ -9,3 +9,21 @@ docker run -itd \
 mysql:latest \
 --character-set-server=utf8mb4 \
 --collation-server=utf8mb4_unicode_ci 
+
+# [mysqld]
+# skip-log-bin
+#
+# echo 'skip-log-bin' >> /etc/mysql/conf.d/docker.cnf
+#
+#show variables like 'log_bin'; 
+#
+#purge master logs before 'yyyy-MM-dd 00:00:00';
+#
+#show variables like 'expire_logs_days';
+#set global expire_logs_days = 30;
+#show binary logs;
+#
+#purge binary logs to 'mysql-bin.000009';
+#reset master;
+
+
