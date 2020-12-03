048c90cb039d035a6a25d93aba784f0b5c680e67
demondevilhades <demonhades@foxmail.com>
2020-12-03 17:47:45 +0800

    Update build.sh

--- dockerfile/jenkins_blueocean_maven_ansible/build.sh
+++ dockerfile/jenkins_blueocean_maven_ansible/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+# tar -zxvf apache-maven-3.6.3-bin.tar.gz
+# sed -i '159i\    </mirror>' apache-maven-3.6.3/conf/settings.xml
+# sed -i '159i\        <mirrorOf>central</mirrorOf>' apache-maven-3.6.3/conf/settings.xml
+# sed -i '159i\        <url>https://maven.aliyun.com/repository/central</url>' apache-maven-3.6.3/conf/settings.xml
+# sed -i '159i\        <name>aliyun maven</name>' apache-maven-3.6.3/conf/settings.xml
+# sed -i '159i\        <id>alimaven</id>' apache-maven-3.6.3/conf/settings.xml
+# sed -i '159i\    <mirror>' apache-maven-3.6.3/conf/settings.xml
+# sed -i '55i\  <localRepository>/home/maven/repo</localRepository>' apache-maven-3.6.3/conf/settings.xml
+
 docker build -t jenkinsci/blueocean-maven-ansible:latest .
 
