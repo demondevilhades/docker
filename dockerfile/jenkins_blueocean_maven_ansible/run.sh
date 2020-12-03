459e631f2f615f2f58321fe0e1d00a9254d02ab0
demondevilhades <demonhades@foxmail.com>
2020-12-03 17:14:46 +0800

    add jenkins_blueocean_maven_ansible

---
+++ dockerfile/jenkins_blueocean_maven_ansible/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p /data/jenkins_blueocean/data
+mkdir -p /data/jenkins_blueocean/home
+
+chown -R 1000:1000 /data/jenkins_blueocean/data
+chown -R 1000:1000 /data/jenkins_blueocean/home
+
+echo "Asia/Hong_Kong" > /etc/timezone
+
+docker-compose up -d
+
