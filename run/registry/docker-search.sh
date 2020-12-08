b7e59e47177964e06133bd55fafcd18b2bba4a54
demondevilhades <demonhades@foxmail.com>
2020-12-08 15:53:45 +0800

    Update docker-search.sh

--- run/registry/docker-search.sh
+++ run/registry/docker-search.sh
@@ -4,19 +4,7 @@
 
 base_url="http://{ip}:5000/v2/"
 
-str=$(curl -s -S "${base_url}_catalog" |  sed -e 's/,/,\n/g' -e 's/\[/\[\n/g' | sed '/repositories/d' | awk -F\" '{print $2;}' | sort -fu)
-repositorys=(${str})
-
-#for repository in ${repositorys[@]}
-#do
-##  echo "${repository}"
-#  curl -s -S "${base_url}${repository}/tags/list" | \
-#    sed -e 's/,/,\n/g' -e 's/\[/\[\n/g' | \
-#    grep '"tags"' | \
-#    awk -F\" '{print $4;}' | \
-#    sort -fu | \
-#    sed -e "s/^/${repository}:/"
-#done
+repositorys=$(curl -s -S "${base_url}_catalog" |  sed -e 's/,/,\n/g' -e 's/\[/\[\n/g' | sed '/repositories/d' | awk -F\" '{print $2;}' | sort -fu)
 
 for repository in ${repositorys[@]}
 do
