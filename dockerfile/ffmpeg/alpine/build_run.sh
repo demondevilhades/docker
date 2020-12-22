3737d0a55b52effd9767775ffc17e7bb9d01c31d
demondevilhades <demonhades@foxmail.com>
2020-12-22 15:20:08 +0800

    Update build_run.sh

--- dockerfile/ffmpeg/alpine/build_run.sh
+++ dockerfile/ffmpeg/alpine/build_run.sh
@@ -2,3 +2,5 @@
 docker build -t ffmpeg-alpine .
 
 docker run --rm ffmpeg-alpine ffmpeg -version
+
+docker run --rm -v ${VIDEO_PATH}:/data ffmpeg-alpine ffmpeg -i /data/${VIDEO_FILE} -codec:v libx264 -codec:a aac -hls_time 10 -hls_list_size 0 -strict -2 -f hls /data/${VIDEO_FILE_PATH}/hls.m3u8
