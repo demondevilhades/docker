74bcacdf849ec457d18c88b17ec47456ef500e32
demondevilhades <demonhades@foxmail.com>
2021-05-07 16:45:28 +0800

    Update run.sh

--- dockerfile/you-get/run.sh
+++ dockerfile/you-get/run.sh
@@ -5,3 +5,11 @@ docker run --rm you-get:3-alpine -i "<url>"
 # download
 docker run --rm -v ~/Downloads:/data/download you-get:3-alpine <download-with> "<url>"
 
+### youku
+docker run --rm -v C:\zs\download\you-get:/data/download you-get:3-alpine --format=mp4hd2v2 "<url.lang><url>"
+
+### qq
+
+
+### iqiyi
+
